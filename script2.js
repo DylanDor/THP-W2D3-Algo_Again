3d3c5685dd4dad5b0054cfb8c74456406f815458
Dylan D <dore.dylan31@gmail.com>
2023-01-11 16:43:43 +0100

    first commit

---
+++ script2.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const fileName = process.argv[2];
+
+const data = fs.readFileSync(fileName, 'utf8', (error, data) => {
+  if (error) {
+      console.error(error.message);
+      return ;
+  }
+}).split(' ').map(string => parseInt(string));
+
+class Comparator{
+  constructor() {
+    this.number = 0
+  }
+}
+
+const comparator = new Comparator;
